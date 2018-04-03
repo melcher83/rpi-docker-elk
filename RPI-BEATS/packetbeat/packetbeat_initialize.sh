acf0cdde21bc76d4d35bd50fc530de8c5d6658a2
Your Name <you@example.com>
2018-04-03 20:13:35 +0000

    moved tetmplate files

--- RPI-BEATS/packetbeat/packetbeat_initialize.sh
+++ RPI-BEATS/packetbeat/packetbeat_initialize.sh
@@ -1,2 +1,2 @@
 curl -XPUT -H 'Content-Type: application/json' http://10.1.8.20:9200/_template/filebeat-7.0.0-alpha1 -d@packetbeat.template.json
-curl -XDELETE 'http://10.1.8.20wq:9200/packetbeat-*'
+curl -XDELETE 'http://10.1.8.20:9200/packetbeat-*'
